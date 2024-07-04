09c4a0aecff934b6bb91787c10a4c9ed5499a737
ashley3214 <68286060+ashley3214@users.noreply.github.com>
2024-07-04 22:23:18 +0300

    loaded character to screen

--- main.py
+++ main.py
@@ -23,9 +23,30 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
     path = join('assets',dir1,dir2)
     images = [f for f in listdir(path) if isfile(join(path, f))]
 
+    all_sprites={}
+
+    for image in images:
+        sprite_sheet= pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites=[]
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width,height),pygame.SRCALPHA,32)
+            rect = pygame.Rect(i*width,0,width,height)
+            surface.blit(sprite_sheet,(0,0),rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace('.png', '') + '_right'] = sprites
+            all_sprites[image.replace('.png', '') + '_left'] = flip(sprites)
+        else:
+            all_sprites[image.replace('.png','')]=sprites
+
+        return all_sprites
+
 class Player(pygame.sprite.Sprite):
     COLOR=(255,0,0)
-    GRAVITY=1
+    GRAVITY=1 #
+    SPRITES= load_sprite_sheets('MainCharacters','MaskDude',32, 32, True)
 
     def __init__(self,x,y,width,height):
         self.rect= pygame.Rect(x,y,width,height)
@@ -38,7 +59,7 @@ class Player(pygame.sprite.Sprite):
 
     def move(self, dx, dy):
         self.rect.x += dx
-        self.rect.y +=dy
+        self.rect.y += dy
 
     def move_left(self, vel):
         self.x_vel= -vel
@@ -53,14 +74,17 @@ class Player(pygame.sprite.Sprite):
             self.animation_count=0
 
     def loop(self, fps):
-        self.y_vel += min(1,( self.fall_count / fps) * self.GRAVITY)
+       #self.y_vel += min(1,( self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count +=1
 
 
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_"+ self.direction][0]
+        win.blit(self.sprite, (self.rect.x,self.rect.y))
+        
+
 
 
 def get_background(name):
@@ -98,7 +122,7 @@ def handle_move(player):
 
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image =get_background('Green.png')
+    background, bg_image =get_background('Pink.png')
 
     player=Player(100,100,50,50)
 
