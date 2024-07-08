e35eec9186305f35cee3c4e7b19c9a41826b9c3f
ashley3214 <68286060+ashley3214@users.noreply.github.com>
2024-07-08 23:27:02 +0300

    fixed bug

--- main.py
+++ main.py
@@ -9,61 +9,61 @@ pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-# g = G*M/R^2,(gravity*mass/distance**2)
 WIDTH, HEIGHT = 1000, 600
 FPS = 60
-PLAYER_VEL= 5
+PLAYER_VEL = 5
 
-window = pygame.display.set_mode((WIDTH,HEIGHT))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def flip(sprites):
-    return [pygame.transform.flip(sprite,True,False)for sprite in sprites]
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
 def load_sprite_sheets(dir1, dir2, width, height, direction=False):
-    path = join('assets',dir1,dir2)
+    path = join('assets', dir1, dir2)
     images = [f for f in listdir(path) if isfile(join(path, f))]
 
-    all_sprites={}
+    all_sprites = {}
 
     for image in images:
-        sprite_sheet= pygame.image.load(join(path, image)).convert_alpha()
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
 
-        sprites=[]
+        sprites = []
         for i in range(sprite_sheet.get_width() // width):
-            surface = pygame.Surface((width,height),pygame.SRCALPHA,32)
-            rect = pygame.Rect(i*width,0,width,height)
-            surface.blit(sprite_sheet,(0,0),rect)
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
             sprites.append(pygame.transform.scale2x(surface))
 
         if direction:
             all_sprites[image.replace('.png', '') + '_right'] = sprites
             all_sprites[image.replace('.png', '') + '_left'] = flip(sprites)
         else:
-            all_sprites[image.replace('.png','')]=sprites
+            all_sprites[image.replace('.png', '')] = sprites
 
     return all_sprites
 
 def get_block(size):
-    path= join('assets', 'Terrain', 'Terrain.png')
+    path = join('assets', 'Terrain', 'Terrain.png')
     image = pygame.image.load(path).convert_alpha()
-    surface =pygame.Surface((size, size), pygame.SRACALPHA,32)
-    rect=pygame.Rect(96,0,size,size)
-    surface.blit(image,(0,0), rect)
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
     return pygame.transform.scale2x(surface)
 
 class Player(pygame.sprite.Sprite):
-    COLOR=(255,0,0)
-    GRAVITY=1 #
-    SPRITES= load_sprite_sheets('MainCharacters','PinkMan',32, 32, True)
+    COLOR = (255, 0, 0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets('MainCharacters', 'PinkMan', 32, 32, True)
     ANIMATION_DELAY = 4
 
-    def __init__(self,x,y,width,height):
-        self.rect= pygame.Rect(x,y,width,height)
-        self.x_vel=0
-        self.y_vel=0
-        self.mask=None
-        self.direction='left'
-        self.animation_count=0
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = 'left'
+        self.animation_count = 0
         self.fall_count = 0
 
     def move(self, dx, dy):
@@ -71,32 +71,32 @@ class Player(pygame.sprite.Sprite):
         self.rect.y += dy
 
     def move_left(self, vel):
-        self.x_vel= -vel
+        self.x_vel = -vel
         if self.direction != 'left':
-            self.direction='left'
-            self.animation_count=0
+            self.direction = 'left'
+            self.animation_count = 0
 
     def move_right(self, vel):
-        self.x_vel= vel
+        self.x_vel = vel
         if self.direction != 'right':
-            self.direction='right'
-            self.animation_count=0
+            self.direction = 'right'
+            self.animation_count = 0
 
     def loop(self, fps):
-       #self.y_vel += min(1,( self.fall_count / fps) * self.GRAVITY)
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
-        self.fall_count +=1
+        self.fall_count += 1
         self.update_sprite()
 
     def update_sprite(self):
-        sprite_sheet= 'idle'
+        sprite_sheet = 'idle'
         if self.x_vel != 0:
-            sprite_sheet='run'
-        
+            sprite_sheet = 'run'
+
         sprite_sheet_name = sprite_sheet + '_' + self.direction
         sprites = self.SPRITES[sprite_sheet_name]
-        sprite_index= (self.animation_count // self.ANIMATION_DELAY) % len(sprites) 
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
         self.sprite = sprites[sprite_index]
         self.animation_count += 1
         self.update()
@@ -106,46 +106,41 @@ class Player(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.sprite)
 
     def draw(self, win):
-        win.blit(self.sprite, (self.rect.x,self.rect.y))
-       
-        
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
+
 class Object(pygame.sprite.Sprite):
-    def __init__(self,x,y,width,height,name=None):
+    def __init__(self, x, y, width, height, name=None):
         super().__init__()
-        self.rect = pygame.Rect(x,y,width,height)
-        self.image = pygame.Surface((width,height),pygame.SRCALPHA)
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
         self.width = width
         self.height = height
         self.name = name
 
-        def draw(self,win):
-            win.blit(self.image, (self.rect.x, self.rect.y))
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
 
-class Block(object):
-    def __init__(self, x ,y , size):
-        super().__init__(x, y , size, size)
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
         block = get_block(size)
-        self.image.blit(block, (0,0))
-        self.mask =pygame.mask.from_surface(self.image)
-
-
-
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
 
 def get_background(name):
-    image=pygame.image.load(join('assets','Background',name))
-    _,_, width,height=image.get_rect()
-    tiles=[]
+    image = pygame.image.load(join('assets', 'Background', name))
+    _, _, width, height = image.get_rect()
+    tiles = []
 
-    for i in range(WIDTH//width + 1):
-        for j in range(HEIGHT// height +1):
-            pos =(i*width, j *height)
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
             tiles.append(pos)
     return tiles, image
 
-
-def draw(window, background,bg_image,player, objects):
+def draw(window, background, bg_image, player, objects):
     for tile in background:
-        window.blit(bg_image,tile)
+        window.blit(bg_image, tile)
 
     for obj in objects:
         obj.draw(window)
@@ -154,26 +149,23 @@ def draw(window, background,bg_image,player, objects):
 
     pygame.display.update()
 
-
 def handle_move(player):
     keys = pygame.key.get_pressed()
 
-    player.x_vel =0
+    player.x_vel = 0
 
-#key been pressed a or k_LEFT
     if keys[pygame.K_LEFT]:
         player.move_left(PLAYER_VEL)
     if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
 
-
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image =get_background('Stone.png')
-    
+    background, bg_image = get_background('Stone.png')
+
     block_size = 96
 
-    player=Player(100,100,50,50)
+    player = Player(100, 100, 50, 50)
     blocks = [Block(0, HEIGHT - block_size, block_size)]
 
     run = True
@@ -182,16 +174,15 @@ def main(window):
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run=False
+                run = False
                 break
+
         player.loop(FPS)
         handle_move(player)
-        draw(window,background,bg_image,player,blocks)
-
+        draw(window, background, bg_image, player, blocks)
 
     pygame.quit()
     quit()
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main(window)
